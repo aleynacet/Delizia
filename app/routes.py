11784df3066177b252a71f04a657ac02a94fb2ff
Arathi-Suwatharan <s_suwatharan22@hwr-berlin.de>
2024-06-14 15:51:08 +0200

    commit

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 
 from flask import render_template
-from app.init import app  
+from app.init import app  # Import von app.init, um die Flask-App zu verwenden
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
--- run.py
+++ run.py
@@ -1,9 +1,8 @@
-
-
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-from app.init import app  
+from app.init import app  # Import von app.init
+
 if __name__ == '__main__':
     app.run(debug=True)
